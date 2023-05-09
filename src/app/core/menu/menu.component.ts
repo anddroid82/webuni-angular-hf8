fde703e56f49e04d511bb404e51c39d3c64f88d7
anddroid82 <anddroid82@gmail.com>
2023-05-09 15:28:41 +0200

    hf8

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+
+  }
+  
+  ngOnInit(): void {
+    this.authService.currentUser.subscribe( user => {
+      if (user !== undefined) {
+        this.isLoggedIn = true;
+      }else{
+        this.isLoggedIn = false;
+      }
+    })
+  }
+
+  logout() {
+    this.authService.logout().subscribe();
+  }
 
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseURL: 'https://us-central1-webuni-angular-course-2021.cloudfunctions.net/api/auth/registration'
+    baseURL: '/api'
 };
