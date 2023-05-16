6b282a876b4baac5b71910781e1f491c4d51dffc
anddroid82 <anddroid82@gmail.com>
2023-05-16 13:49:46 +0200

    hf9

--- src/app/ideas/new-idea/new-idea.component.ts
+++ src/app/ideas/new-idea/new-idea.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IdeasService } from '../ideas.service';
 import { Router } from '@angular/router';
 import { Idea } from '../models/idea.model';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-idea',
@@ -12,7 +12,10 @@ import { FormGroup } from '@angular/forms';
 export class NewIdeaComponent {
 
   inProgress:boolean=false;
-  form:FormGroup = new FormGroup()
+  form:FormGroup = new FormGroup({
+    name: new FormControl('',Validators.required),
+    description: new FormControl('',Validators.required)
+  });
 
   constructor(private ideaService: IdeasService, private router: Router) { }
 
@@ -22,7 +25,7 @@ export class NewIdeaComponent {
 
   addIdea(){
     this.inProgress=true;
-    const idea:Idea = new Idea('',this.name,this.description,0);
+    const idea:Idea = new Idea('',this.form.value.name,this.form.value.description,0);
     this.ideaService.addIdea(idea).subscribe( i => {
       this.router.navigateByUrl('/ideas');
     });
