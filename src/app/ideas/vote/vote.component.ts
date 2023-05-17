84201a00e5e2cbcd726b30ed25b3ee675cc13438
anddroid82 <anddroid82@gmail.com>
2023-05-17 14:15:46 +0200

    hf9

--- src/app/ideas/vote/vote.component.ts
+++ src/app/ideas/vote/vote.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Idea } from '../models/idea.model';
+import { IdeasService } from '../ideas.service';
 
 @Component({
   selector: 'app-vote',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./vote.component.scss']
 })
 export class VoteComponent {
+
+  @Input() idea:Idea|undefined = undefined;
+
+  constructor(private ideasService:IdeasService) {
+
+  }
+
+  upVote() {
+
+  }
+
+  downVote(){
+
+  }
 
 }
